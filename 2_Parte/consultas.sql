1433712d4cbc1a84a54dd556c6702b65885b09b5
Caio Andrade <caiotdandrade@gmail.com>
2020-05-31 17:06:47 -0300

    Adiciona terceira consulta

--- 2_Parte/consultas.sql
+++ 2_Parte/consultas.sql
@@ -8,3 +8,18 @@ WHERE e.id_exame = rea.id_exame AND e.id_exame = reg.id_exame
 -- Liste os 5 exames realizados com maior eficiencia (isto é, menor diferença de tempo entre 
 -- data de realização e data de solicitação)
 
+-- Liste os servições que podem ser realizados pelos usuários
+
+SELECT usuario.nome, servico.classe
+FROM 
+    usuario
+    perfil,
+	possui,
+	pertence,
+    servico
+WHERE
+    usuario.id_usuario = possui.id_usuario AND
+	possui.id_perfil = perfil.id_perfil AND
+	perfil.id_perfil = pertence.id_perfil AND
+	pertence.id_servico = servico.id_servico
+
