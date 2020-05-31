64c751d977c244df439f36d7df493139c50cc899
Caio Andrade <caiotdandrade@gmail.com>
2020-05-31 16:28:09 -0300

    Corrige primeira consulta
    Exibe nome do paciente ao invés do id do paciente.
    Corrige alias da tabela registro
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- 2_Parte/consultas.sql
+++ 2_Parte/consultas.sql
@@ -1,9 +1,9 @@
 -- Lista todos exames realizados, com seus respectivos tipos, bem como os seus usuários com suas
 -- respectivas datas de solitação e execução
 
-SELECT e.tipo, reg.data_de_solicitacao, rea.data_de_realizacao, r2.id_usuario 
-FROM exame as e, realiza as rea, registro as r2, 
-WHERE e.id_exame = rea.id_exame AND e.id_exame = reg.id_exame
+SELECT e.tipo, rea.data_de_realizacao, u.nome, reg.data_de_solicitacao
+FROM exame as e, realiza as rea, registro as reg, usuario as u
+WHERE e.id_exame = rea.id_exame AND reg.id_exame = e.id_exame
 
 -- Liste os 5 exames realizados com maior eficiencia (isto é, menor diferença de tempo entre 
 -- data de realização e data de solicitação)
