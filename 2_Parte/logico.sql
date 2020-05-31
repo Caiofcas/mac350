80f82963c52c596a1368ddc437ac50658b3d7d0f
Caio Andrade <caiotdandrade@gmail.com>
2020-05-31 14:55:28 -0300

    Troca ordem das coisas no modelo logico para bater com arquivo de insercao

--- 2_Parte/logico.sql
+++ 2_Parte/logico.sql
@@ -1,3 +1,4 @@
+-- Classes simples
 
 CREATE TABLE usuario (
     id_usuario    INT NOT NULL PRIMARY KEY,
@@ -19,36 +20,11 @@ CREATE TABLE perfil (
 	UNIQUE (codigo)
 );
 
---Relacionamento possui
-CREATE TABLE possui (
-    id_usuario    INT NOT NULL references usuario(id_usuario),
-    id_perfil    INT NOT NULL references perfil(id_perfil), 
-	UNIQUE (id_usuario, id_perfil)
-);
-
 CREATE TABLE servico (
     id_servico    INT NOT NULL PRIMARY KEY,
     nome    VARCHAR(255) NOT NULL,
     classe    VARCHAR(255) NOT NULL CHECK (classe IN ('visualização', 'inserção', 'alteração', 'remoção')),
 	UNIQUE (nome, classe)
-);
-
---Relacionamento pertence
-CREATE TABLE pertence (
-    id_servico    INT NOT NULL references servico(id_servico),
-    id_perfil    INT NOT NULL references perfil(id_perfil),
-	UNIQUE (id_servico, id_perfil)
-);
-
---Relacionamento tutelamento
-CREATE TABLE tutelamento (
-    id_usuario_tutelado    INT NOT NULL references usuario(id_usuario),
-    id_tutor    INT NOT NULL references usuario(id_usuario),
-    id_servico    INT NOT NULL references servico(id_servico),
-    id_perfil    INT NOT NULL references perfil(id_perfil),
-    data_de_inicio    DATE NOT NULL,
-    data_de_termino    DATE,
-	UNIQUE (id_usuario_tutelado, id_tutor, id_servico, id_perfil)
 );
 
 CREATE TABLE paciente (
@@ -60,11 +36,26 @@ CREATE TABLE paciente (
 	UNIQUE (cpf)
 );
 
-CREATE TABLE exame (
-    id_exame    INT NOT NULL PRIMARY KEY,
-    tipo    VARCHAR(255) NOT NULL,
-    virus    VARCHAR(255) NOT NULL,
-	UNIQUE (tipo, virus)
+CREATE TABLE exame {
+    id_exame INT NOT NULL PRIMARY KEY,
+    tipo VARCHAR(255) NOT NULL,
+    virus VARCHAR(255) NOT NULL,
+}
+
+--Relacionamentos
+
+--Relacionamento possui
+CREATE TABLE possui (
+    id_usuario    INT NOT NULL references usuario(id_usuario),
+    id_perfil    INT NOT NULL references perfil(id_perfil), 
+	UNIQUE (id_usuario, id_perfil)
+);
+
+--Relacionamento pertence
+CREATE TABLE pertence (
+    id_servico    INT NOT NULL references servico(id_servico),
+    id_perfil    INT NOT NULL references perfil(id_perfil),
+	UNIQUE (id_servico, id_perfil)
 );
 
 --Relacionamento gerencia
@@ -72,6 +63,18 @@ CREATE TABLE gerencia (
     id_servico    INT NOT NULL references servico(id_servico),
     id_exame    INT NOT NULL references exame(id_exame),
 	UNIQUE (id_servico, id_exame)
+);
+
+
+--Relacionamento tutelamento
+CREATE TABLE tutelamento (
+    id_usuario_tutelado    INT NOT NULL references usuario(id_usuario),
+    id_tutor    INT NOT NULL references usuario(id_usuario),
+    id_servico    INT NOT NULL references servico(id_servico),
+    id_perfil    INT NOT NULL references perfil(id_perfil),
+    data_de_inicio    DATE NOT NULL,
+    data_de_termino    DATE,
+	UNIQUE (id_usuario_tutelado, id_tutor, id_servico, id_perfil)
 );
 
 --Relacionamento realiza
@@ -92,6 +95,8 @@ CREATE TABLE amostra (
     material    VARCHAR(255) NOT NULL,
 	UNIQUE (id_paciente, id_exame, codigo_amostra)
 );
+
+-- Relacionamentos agregados
 
 --Relacionamento registro
 CREATE TABLE registro (
