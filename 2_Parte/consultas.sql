3bc187fd6ad0055d4a81d823a37d62f6bfde25cc
Caio Andrade <caiotdandrade@gmail.com>
2020-05-31 16:31:54 -0300

    Corrige ordem das colunas pra fazer mais sentido na consulta 1

--- 2_Parte/consultas.sql
+++ 2_Parte/consultas.sql
@@ -1,9 +1,9 @@
 -- Lista todos exames realizados, com seus respectivos tipos, bem como os seus usuários com suas
 -- respectivas datas de solitação e execução
 
-SELECT e.tipo, rea.data_de_realizacao, u.nome, reg.data_de_solicitacao
+SELECT e.tipo, reg.data_de_solicitacao, rea.data_de_realizacao, u.nome
 FROM exame as e, realiza as rea, registro as reg, usuario as u
-WHERE e.id_exame = rea.id_exame AND reg.id_exame = e.id_exame
+WHERE e.id_exame = rea.id_exame AND e.id_exame = reg.id_exame
 
 -- Liste os 5 exames realizados com maior eficiencia (isto é, menor diferença de tempo entre 
 -- data de realização e data de solicitação)
