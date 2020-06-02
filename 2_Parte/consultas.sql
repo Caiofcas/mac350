caafb5aa63a779ba9c6dda0cd282cafbb89d8cdc
Caio <caiofcastro@usp.br>
2020-06-02 19:10:10 -0300

    small chanes + 4.2

--- 2_Parte/consultas.sql
+++ 2_Parte/consultas.sql
@@ -1,14 +1,23 @@
--- Lista todos exames realizados, com seus respectivos tipos, bem como os seus usuários com suas
+-- 4.1) Lista todos exames realizados, com seus respectivos tipos, bem como os seus usuários com suas
 -- respectivas datas de solitação e execução
 
 SELECT e.tipo, reg.data_de_solicitacao, rea.data_de_realizacao, u.nome
 FROM exame as e, realiza as rea, registro as reg, usuario as u
 WHERE e.id_exame = rea.id_exame AND e.id_exame = reg.id_exame
 
--- Liste os 5 exames realizados com maior eficiencia (isto é, menor diferença de tempo entre 
+-- 4.2) Liste os 5 exames realizados com maior eficiencia (isto é, menor diferença de tempo entre 
 -- data de realização e data de solicitação)
 
--- Liste os servições que podem ser realizados pelos usuários
+SELECT rea.id_exame, rea.data_de_realizacao, reg.data_de_solicitacao
+FROM realiza as rea, registro as reg
+WHERE rea.id_exame = reg.id_exame
+ORDER BY 
+	DATE_PART('month', rea.data_de_realizacao - reg.data_de_solicitacao) * 24 * 31 +
+	DATE_PART('day', rea.data_de_realizacao - reg.data_de_solicitacao) * 24 + 
+	DATE_PART('hour', rea.data_de_realizacao - reg.data_de_solicitacao)
+LIMIT 5;
+
+-- 4.3) Liste os serviços que podem ser realizados pelos usuários
 
 SELECT usuario.nome, servico.classe
 FROM 
@@ -23,3 +32,7 @@ WHERE
 	perfil.id_perfil = pertence.id_perfil AND
 	pertence.id_servico = servico.id_servico
 
+-- 4.4) Liste os serviços que podem ser utilizados por usuários tutelados (dependentes)
+
+-- 4.5) Liste em ordem crescente o total de serviços utilizados agrupados pelos tipos de
+-- serviços disponíveis e pelo perfil dos usuários.
