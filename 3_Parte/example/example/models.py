5f9500b9865639374fb7e88e14e3c68c44a77513
Caio <caiofcastro@usp.br>
2020-06-29 23:32:56 -0300

    adds Pessoa and Django classes

--- 3_Parte/example/example/models.py
+++ 3_Parte/example/example/models.py
@@ -1,11 +1,46 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
+# Tabelas simples
+
+class Pessoa(models.Model):
+    id_pessoa = models.AutoField(primary_key=True)
+    cpf = models.CharField(unique=True, max_length=11)
+    nome = models.CharField(max_length=255)
+    data_de_nascimento = models.DateField(blank=True, null=True)
+    endereco = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'pessoa'
+
+    def __str__(self):
+        return self.nome
+
+class Usuario(models.Model):
+    id_usuario = models.IntegerField(primary_key=True)
+    cpf_pessoa = models.OneToOneField(Pessoa, models.DO_NOTHING, db_column='cpf_pessoa')
+    area_de_pesquisa = models.CharField(max_length=255, blank=True, null=True)
+    instituicao = models.CharField(max_length=255, blank=True, null=True)
+    login = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
+    id_tutor = models.ForeignKey('self', models.DO_NOTHING, db_column='id_tutor', blank=True, null=True)
+
+    class Meta:
+        db_table = 'usuario'
+
+    def __str__(self):
+        return self.login
+
+class Paciente(models.Model):
+    id_paciente = models.AutoField(primary_key=True)
+    cpf_pessoa = models.OneToOneField('Pessoa', models.DO_NOTHING, db_column='cpf_pessoa')
+
+    class Meta:
+        managed = False
+        db_table = 'paciente'
+
+    def __str__(self):
+        return str(self.cpf_pessoa)
 
 class Exame(models.Model):
     id_exame = models.AutoField(primary_key=True)
@@ -13,9 +48,9 @@ class Exame(models.Model):
     virus = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
         db_table = 'exame'
         unique_together = (('tipo', 'virus'),)
+
     def __str__(self):
         return self.tipo
 
@@ -24,10 +59,49 @@ class Perfil(models.Model):
     codigo = models.CharField(unique=True, max_length=255)
     tipo = models.CharField(max_length=255, blank=True, null=True)
 
+    class Meta:
+        db_table = 'perfil'
+
     def __str__(self):
         return self.tipo
-    
 
+class Registro(models.Model):
+    id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario')
+    id_servico = models.ForeignKey('Servico', models.DO_NOTHING, db_column='id_servico')
+    id_exame = models.ForeignKey(Exame, models.DO_NOTHING, db_column='id_exame')
+
+    class Meta:
+        managed = False
+        db_table = 'registro'
+        unique_together = (('id_servico', 'id_usuario', 'id_exame'),)
+
+class Servico(models.Model):
+    id_servico = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=255)
+    classe = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'servico'
+        unique_together = (('nome', 'classe'),)
+
+    def __str__(self):
+        return self.nome
+
+# Agregados
+
+class Amostra(models.Model):
+    id_paciente = models.ForeignKey('Paciente', models.DO_NOTHING, db_column='id_paciente')
+    id_exame = models.ForeignKey('Exame', models.DO_NOTHING, db_column='id_exame')
+    codigo_amostra = models.CharField(max_length=255)
+    metodo_de_coleta = models.CharField(max_length=255)
+    material = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'amostra'
+        unique_together = (('id_paciente', 'id_exame', 'codigo_amostra'),)
+
+# Relações
 
 class Gerencia(models.Model):
     id_servico = models.ForeignKey('Servico', models.DO_NOTHING, db_column='id_servico')
@@ -38,21 +112,6 @@ class Gerencia(models.Model):
         db_table = 'gerencia'
         unique_together = (('id_servico', 'id_exame'),)
 
-
-class Paciente(models.Model):
-    id_paciente = models.AutoField(primary_key=True)
-    cpf = models.CharField(unique=True, max_length=11)
-    nome = models.CharField(max_length=255)
-    endereco = models.CharField(max_length=255)
-    nascimento = models.DateField()
-
-    class Meta:
-        managed = False
-        db_table = 'paciente'
-    def __str__(self):
-        return "{} - {}".format(self.nome, self.cpf)
-
-
 class Pertence(models.Model):
     id_servico = models.ForeignKey('Servico', models.DO_NOTHING, db_column='id_servico')
     id_perfil = models.ForeignKey(Perfil, models.DO_NOTHING, db_column='id_perfil')
@@ -62,7 +121,6 @@ class Pertence(models.Model):
         db_table = 'pertence'
         unique_together = (('id_servico', 'id_perfil'),)
 
-
 class Possui(models.Model):
     id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario')
     id_perfil = models.ForeignKey(Perfil, models.DO_NOTHING, db_column='id_perfil')
@@ -71,7 +129,6 @@ class Possui(models.Model):
         managed = False
         db_table = 'possui'
         unique_together = (('id_usuario', 'id_perfil'),)
-
 
 class Realiza(models.Model):
     id_paciente = models.ForeignKey(Paciente, models.DO_NOTHING, db_column='id_paciente')
@@ -84,31 +141,6 @@ class Realiza(models.Model):
         managed = False
         db_table = 'realiza'
         unique_together = (('id_paciente', 'id_exame', 'data_de_realizacao'),)
-
-
-class Registro(models.Model):
-    id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario')
-    id_servico = models.ForeignKey('Servico', models.DO_NOTHING, db_column='id_servico')
-    id_exame = models.ForeignKey(Exame, models.DO_NOTHING, db_column='id_exame')
-
-    class Meta:
-        managed = False
-        db_table = 'registro'
-        unique_together = (('id_servico', 'id_usuario', 'id_exame'),)
-
-
-class Servico(models.Model):
-    id_servico = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255)
-    classe = models.CharField(max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'servico'
-        unique_together = (('nome', 'classe'),)
-    def __str__(self):
-        return self.nome
-
 
 class Tutelamento(models.Model):
     id_usuario_tutelado = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario_tutelado', related_name='id_usuario_tutelado')
@@ -123,37 +155,104 @@ class Tutelamento(models.Model):
         db_table = 'tutelamento'
         unique_together = (('id_usuario_tutelado', 'id_tutor', 'id_servico', 'id_perfil'),)
 
+# Django created tables
 
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    cpf = models.CharField(unique=True, max_length=11)
-    nome = models.CharField(max_length=255)
-    area_de_pesquisa = models.CharField(max_length=255, blank=True, null=True)
-    instituicao = models.CharField(max_length=255, blank=True, null=True)
-    data_de_nascimento = models.DateField(blank=True, null=True)
-    login = models.CharField(max_length=255)
-    senha = models.CharField(max_length=255)
-    perfis = models.ManyToManyField(Perfil, through='Usuario_Possui_Perfil')
-    id_tutor = models.ForeignKey('self', models.DO_NOTHING, db_column='id_tutor', blank=True, null=True)
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
 
     class Meta:
         managed = False
-        db_table = 'usuario'
-    def __str__(self):
-        return self.nome
+        db_table = 'auth_group'
 
-class Usuario_Possui_Perfil(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
-    perfil = models.ForeignKey(Perfil, on_delete=models.PROTECT)
-
-class Amostra(models.Model):
-    id_paciente = models.ForeignKey('Paciente', models.DO_NOTHING, db_column='id_paciente')
-    id_exame = models.ForeignKey('Exame', models.DO_NOTHING, db_column='id_exame')
-    codigo_amostra = models.CharField(max_length=255)
-    metodo_de_coleta = models.CharField(max_length=255)
-    material = models.CharField(max_length=255)
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
 
     class Meta:
         managed = False
-        db_table = 'amostra'
-        unique_together = (('id_paciente', 'id_exame', 'codigo_amostra'),)
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
