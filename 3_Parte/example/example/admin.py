8a16572c50783ea5c1cb3d84c31fe1d23f7701de
Caio <caiofcastro@usp.br>
2020-06-30 15:57:57 -0300

    cleanup admin

--- 3_Parte/example/example/admin.py
+++ 3_Parte/example/example/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-from .models import Usuario, Perfil, Possui, Exame, Servico
+from .models import Usuario, Perfil, Exame, Servico
 
-class PerfilInline(admin.TabularInline):
-    model = Possui
-    extra = 1
-
-class UsuarioAdmin(admin.ModelAdmin):
-    inlines = (PerfilInline,)
-
-admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Usuario)
 admin.site.register(Perfil)
 admin.site.register(Exame)
 admin.site.register(Servico)
-#admin.site.register(Usuario_Possui_Perfil)
